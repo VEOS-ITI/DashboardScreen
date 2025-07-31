588ddd2afa7c6157c592774b2e5deabb304bf60a
Yara-Abdelrahem <yaraomran961@gmail.com>
2025-07-31 17:44:09 +0300

    Handle appearance of icons logic

--- app/src/main/java/com/example/dashboardscreen/MainActivity.java
+++ app/src/main/java/com/example/dashboardscreen/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.dashboardscreen;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
+import android.hardware.lights.Light;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,10 +20,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int vhallighting_status=1,vhalright_light_status=0,vhalleft_light_status =1,vhalbatterylevel=15;
     RecyclerView gearRecycler;
-    ImageView carImage;
-    ImageView road1, road2;
-    float roadHeight = 176f; // match height in dp
+
+    TextView charge_txt;
+
+    ImageView road1, road2 , lightingON ,lightingOFF ,lightingERROR , rightLightON, leftLightON, rightLightOFF, leftLightOFF,batterycharge, batterylevel;
+    float roadHeight = 600f; // match height in dp
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +39,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        lightingON=findViewById(R.id.light_on_img);
+        lightingOFF=findViewById(R.id.light_off_img);
+        lightingERROR=findViewById(R.id.light_error_img);
+        rightLightON= findViewById(R.id.right_on_img);
+        rightLightOFF= findViewById(R.id.right_off_img);
+        leftLightON= findViewById(R.id.left_on_img);
+        leftLightOFF= findViewById(R.id.left_off_img);
+
+        batterycharge = findViewById(R.id.charge_img);
+        batterylevel =findViewById(R.id.battery4_img);
+        charge_txt = findViewById(R.id.charge_txt);
 
         gearRecycler = findViewById(R.id.gearRecycler);
+
         // Use horizontal layout for gear shift
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         gearRecycler.setLayoutManager(layoutManager);
@@ -45,43 +63,10 @@ public class MainActivity extends AppCompatActivity {
 
         adapter.setSelectedPosition(1);
 
-//
-//        ImageView car = findViewById(R.id.carView);
-//
-//// Pulse effect
-//        ObjectAnimator pulse = ObjectAnimator.ofFloat(car, "scaleX", 1f, 1.1f);
-//        pulse.setDuration(500);
-//        pulse.setRepeatMode(ValueAnimator.REVERSE);
-//        pulse.setRepeatCount(1);
-//
-//        ObjectAnimator pulseY = ObjectAnimator.ofFloat(car, "scaleY", 1f, 1.1f);
-//        pulseY.setDuration(500);
-//        pulseY.setRepeatMode(ValueAnimator.REVERSE);
-//        pulseY.setRepeatCount(1);
-//
-//// Forward movement + fade
-//        ObjectAnimator move = ObjectAnimator.ofFloat(car, "translationY", 0, -400f);
-//        move.setDuration(1000);
-//        ObjectAnimator fade = ObjectAnimator.ofFloat(car, "alpha", 1f, 0f);
-//        fade.setDuration(1000);
-//
-//// Play pulse first, then move & fade
-//        AnimatorSet set = new AnimatorSet();
-//        set.play(pulse).with(pulseY);
-//        set.play(move).with(fade).after(pulse);
-//        set.start();
-
-//        ImageView road = findViewById(R.id.roadView);
-//
-//// Move the road background up in a loop
-//        ObjectAnimator roadAnim = ObjectAnimator.ofFloat(road, "translationY", 0f, -400f);
-//        roadAnim.setDuration(2000);
-//        roadAnim.setRepeatCount(ValueAnimator.INFINITE);
-//        roadAnim.setRepeatMode(ValueAnimator.RESTART);
-//        roadAnim.start();
-        road1 = findViewById(R.id.road1);
-        road2 = findViewById(R.id.road2);
-        animateRoad();
+        handleLighting();
+        handleRightLight();
+        handleLeftLight();
+        handleBatteryLevel();
     }
     private void animateRoad() {
         final Handler handler = new Handler();
@@ -107,4 +92,100 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    private void handleLighting(){
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // Background work
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(vhallighting_status == 1) {
+                            lightingON.setVisibility(ImageView.VISIBLE);
+                            lightingOFF.setVisibility(ImageView.INVISIBLE);
+                            lightingERROR.setVisibility(ImageView.INVISIBLE);
+                        } else if(vhallighting_status == 0) {
+                            lightingON.setVisibility(ImageView.INVISIBLE);
+                            lightingOFF.setVisibility(ImageView.VISIBLE);
+                            lightingERROR.setVisibility(ImageView.INVISIBLE);
+                        } else if(vhallighting_status == -1) {
+                            lightingON.setVisibility(ImageView.INVISIBLE);
+                            lightingOFF.setVisibility(ImageView.INVISIBLE);
+                            lightingERROR.setVisibility(ImageView.VISIBLE);
+                        }
+                    }
+                });
+            }
+        }).start();
+    }
+    private void handleRightLight() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // Background work
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(vhalright_light_status == 1) {
+                            rightLightON.setVisibility(ImageView.VISIBLE);
+                            rightLightOFF.setVisibility(ImageView.INVISIBLE);
+                        } else if(vhalright_light_status == 0) {
+                            rightLightON.setVisibility(ImageView.INVISIBLE);
+                            rightLightOFF.setVisibility(ImageView.VISIBLE);
+                        }
+                    }
+                });
+            }
+        }).start();
+    }
+    private void handleLeftLight() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // Background work
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(vhalleft_light_status == 1) {
+                            leftLightON.setVisibility(ImageView.VISIBLE);
+                            leftLightOFF.setVisibility(ImageView.INVISIBLE);
+                        } else if(vhalleft_light_status == 0) {
+                            leftLightON.setVisibility(ImageView.INVISIBLE);
+                            leftLightOFF.setVisibility(ImageView.VISIBLE);
+                        }
+                    }
+                });
+            }
+        }).start();
+    }
+    private void handleBatteryLevel() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // Background work
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        charge_txt.setText(vhalbatterylevel + "%");
+                        if(vhalbatterylevel >= 80) {
+                            batterylevel.setImageResource(R.drawable.battery5);
+                            batterycharge.setVisibility(ImageView.INVISIBLE);
+                        } else if(vhalbatterylevel >= 50) {
+                            batterylevel.setImageResource(R.drawable.battery4);
+                            batterycharge.setVisibility(ImageView.INVISIBLE);
+
+                        } else if(vhalbatterylevel >= 20) {
+                            batterylevel.setImageResource(R.drawable.battery3);
+                            batterycharge.setVisibility(ImageView.INVISIBLE);
+                        } else {
+                            batterylevel.setImageResource(R.drawable.battery2);
+                            batterycharge.setVisibility(ImageView.VISIBLE);
+                        }
+                    }
+                });
+            }
+        }).start();
+    }
+
 }
